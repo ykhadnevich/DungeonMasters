a47c0a6f4698aa575cf4a99fd5664fd7bd698c26
Yan_Khadnevich <90506265+ykhadnevich@users.noreply.github.com>
2024-07-08 21:47:31 +0300

    first commit

---
+++ Assets/Scripts/main.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float3 name; 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
